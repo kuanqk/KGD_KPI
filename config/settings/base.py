6d91d32691acfa30e8d21c120e550357d4cd9fc1
Kuanysh Abeshev <kuanqk@gmail.com>
2026-04-16 12:51:04 +0500

    fix: token_blacklist + nginx (если ещё не коммитил)

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,6 +22,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'django_filters',
     'django_celery_beat',
